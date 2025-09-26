67ea0b43c007fed5c531c1a9a88b1f96da96d986
Ionita Aurel Mihai <ionitaaurel32@gmail.com>
2025-09-26 15:39:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let inputField = document.getElementById("username");
+let ClearBtn = document.getElementById("clear-btn");
+ClearBtn.addEventListener("click", function () {
+  inputField.value = "";
+});
